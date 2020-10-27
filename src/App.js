a50a5e6206e4277334ff1bafe87e561c33079a9c
Daniel Cunha <danielcc.jp@gmail.com>
2020-10-27 10:30:22 -0300

    + conceitos, babel, webpack, componentes, propriedades & estado e imutabilidade

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+import Header from './components/Header';
+
+/**
+ * Componente
+ * Propriedade
+ * Estado & Imutabilidade
+ */
+
+function App() {
+  const [projects, setProjects] = useState([
+    'Desenvolvimento de App',
+    'Front-end web',
+  ]);
+
+  function handleAddProject() {
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  }
+
+  return (
+    <>
+      <Header title="Projects" />
+
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
+    </>
+  );
+}
+
+export default App;
